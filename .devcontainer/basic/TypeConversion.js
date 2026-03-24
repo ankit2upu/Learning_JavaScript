031f095dd184e229a2e20390911b8a1c9bb0efa0
Ankit kumar gupta <ankitgpt2345@gmail.com>
2026-03-24 07:48:03 +0000

    revision conversion in details

--- .devcontainer/basic/TypeConversion.js
+++ .devcontainer/basic/TypeConversion.js
@@ -1,4 +1,8 @@
-// type conversion
+// type conversion  Number(), Boolean(), String();
+
+
+//  Number()
+console.log("**********Number Conversion*************");
 
 let score="73"
 
@@ -12,4 +16,50 @@ console.log(typeof score);   // string
 let score1="abcd "
 let valueInNumber1=Number(score1) 
 console.log(typeof valueInNumber1); // number  
-console.log(valueInNumber1);     //NaN   ** 
+console.log(valueInNumber1);     //NaN *****
+
+
+let x=null
+console.log(x);
+console.log(Number(x));   // 0
+console.log(typeof Number(x));   //number
+
+
+let y=undefined
+console.log(Number(undefined));    // NaN
+console.log(typeof Number(undefined));   // number 
+
+let isTrue=true
+console.log(isTrue);   //true
+console.log(Number(isTrue));   // 1
+console.log(typeof Number(isTrue));   // number
+
+
+/*  NOTE: if typeConversion of string to Number 
+          "33" easy to convert into number print 33
+          "abc" can not convert into number print NaN
+              but type converted
+
+*/ 
+
+ //Boolean()
+console.log("********* Boolean convesion*********");
+
+let logged=1; // number  1->true , 0->false
+console.log(Boolean(logged));  // true
+console.log(typeof Boolean(logged)); //boolean 
+
+let string="Ankit" //"string"-> true," "-> true ""-> false 
+console.log(Boolean(string)); // true
+console.log(typeof Boolean(string));  //boolean
+  
+// String()                           
+console.log("************String conversion*************");
+
+let sum=384
+console.log(String(sum));  //384
+console.log(typeof String(sum));  //string
+
+
+
+
